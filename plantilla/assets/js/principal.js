70df72281961755ccdd4891eb21c646b68e3e113
xoly00 <xolt00@gmail.com>
2021-02-24 20:09:05 +0100

    Transiciones trabajos y links por terminar

--- plantilla/assets/js/principal.js
+++ plantilla/assets/js/principal.js
@@ -7,9 +7,9 @@
 
 (function(){
     var umbral_SCROLL = 200;
-    window.onscroll = function() {myFunction()};
+    window.onscroll = function() {scrollBanner()};
     
-    function myFunction() {
+    function scrollBanner() {
       if (document.body.scrollTop > umbral_SCROLL || document.documentElement.scrollTop > umbral_SCROLL) {
         document.getElementById("header").style.backgroundColor = "#1c1c1c";
       }
@@ -17,4 +17,14 @@
         document.getElementById("header").style.backgroundColor = "";
       }
     }
+    
+    
 })();
+  function hoverlinks(obj){
+    //document.getElementsById("current_page").style.textDecoration = "none";
+    obj.style.textDecoration = "underline";
+  }
+  function nothoverlinks(obj){
+    obj.style.textDecoration = "none";
+  //document.getElementsById("current_page").style.textDecoration = "underline";
+  }
