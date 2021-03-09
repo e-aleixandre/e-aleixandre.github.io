e9a709e89cb19659273a0d8fa985806cbd4fd208
enmonpo <enmonpo@alumni.uv.es>
2021-03-09 10:19:14 +0100

    Añadida duración del vídeo y cambio de volumen

--- plantilla/assets/js/trabajos.js
+++ plantilla/assets/js/trabajos.js
@@ -6,29 +6,44 @@ var trabajos = (function(document){
         video[0].classList.toggle('video-play');
         video[0].play();
     }
+
     function pauseVideo(){
         var video = this.parentNode.parentNode.getElementsByTagName('video');
         video[0].pause();
         video[0].classList.toggle('video-play');
     }
+
     function stopVideo(){
         var video = this.parentNode.parentNode.getElementsByTagName('video');
         video[0].pause();
         video[0].currentTime = 0;
         video[0].classList.toggle('video-play');
     }
+
     function backwardVideo(){
         var video = this.parentNode.parentNode.getElementsByTagName('video');
         video[0].currentTime = video[0].currentTime - 2;
     }
+
     function forwardVideo(){
         var video = this.parentNode.parentNode.getElementsByTagName('video');
         video[0].currentTime = video[0].currentTime + 2;
     }
+
     function fullscreenVideo(){
         this.parentNode.parentNode.getElementsByTagName('video')[0].requestFullscreen();
     }
-    
+
+    function updateTimeVideo(event){
+        var timeDiv = document.getElementById("tiempo-video");
+        timeDiv.innerText = getVideoDurationString(event.target);
+    }
+
+    function changeVolumeVideo(event) {
+        var video = event.target.parentNode.parentNode.parentNode.querySelector("video");
+        video.volume = event.target.value / 100;
+    }
+
     function switchVideoStatus(){
         if(this.paused){
             this.play();
@@ -43,6 +58,33 @@ var trabajos = (function(document){
         //this.duration;
     }
 
+    // Información del vídeo en un formato adecuado
+    function getVideoDurationString(video) {
+      var duration = video.duration - video.currentTime,
+          mins,
+          secs;
+
+      secs = duration % 60;
+      mins = (duration - secs) / 60;
+
+      secs = Math.ceil(secs);
+
+      mins = addLeadingZeros(mins);
+      secs = addLeadingZeros(secs);
+
+      return mins + ":" + secs;
+    }
+
+    function addLeadingZeros(num) {
+      num = num.toString();
+
+      while (num.length < 2) {
+        num = "0" + num;
+      }
+
+      return num;
+    }
+
     var controles_video = document.getElementsByClassName("controles-video");
     for(var i = 0; i < controles_video.length; i++)
     {
@@ -54,18 +96,23 @@ var trabajos = (function(document){
         controles_video[i].querySelector('i.fa-expand-arrows-alt').addEventListener('click', fullscreenVideo);
     }
 
-    //Selección de todos los objetos tipo vídeo
+    // Event Listeners de los vídeos
     var video_elements = document.getElementsByTagName('video');
     for(var j = 0; j < controles_video.length; j++)
     {
         video_elements[j].addEventListener('click', switchVideoStatus);
         video_elements[j].addEventListener('loadeddata', setVideoInformation);
+        video_elements[j].addEventListener('timeupdate', updateTimeVideo);
     }
-    
-    
+
+    var volumenes = document.querySelectorAll("input.volume");
+
+    for (var i = 0; i < volumenes.length; i++) {
+      volumenes[i].addEventListener('change', changeVolumeVideo);
+    }
 
     //Animación Hover cards
-    function arrowAnimation() {     
+    function arrowAnimation() {
         this.querySelector('i').classList.add('animate__fadeInLeft');
     }
     function arrowAnimationOut(){
